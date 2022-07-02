ae4c0181651f14379b9fbfad2782cc7a566b9903
McMeny <minuadefris@gmail.com>
2022-07-02 18:19:39 +0100

    update

--- menu.py
+++ menu.py
@@ -18,6 +18,7 @@ class Window(QMainWindow):
         snip_btn.setText('Snip')
         snip_btn.resize(80,50)
         layout.addWidget(snip_btn)
+        print('hello')
         snip_btn.clicked.connect(SnipOCR.Snip_tool)
 
         #snipcopy_btn = QPushButton(window)
