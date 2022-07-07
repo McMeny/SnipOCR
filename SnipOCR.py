77dc3028422b77300b09d72c7905aa7c124b9b40
McMeny <minuadefris@gmail.com>
2022-07-07 16:53:24 +0100

    physics & chemistry notes update

--- SnipOCR.py
+++ SnipOCR.py
@@ -13,27 +13,18 @@ from PyQt5.QtCore import QPoint, Qt, QRect
 from PyQt5.QtWidgets import QAction, QMainWindow, QApplication, QPushButton, QMenu, QFileDialog
 from PyQt5.QtGui import QPixmap, QImage, QPainter, QPen
 
-app = QApplication(sys.argv)
-
 class Snip_tool(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        screen = app.primaryScreen()
-        size = screen.size()
-        rect = screen.availableGeometry()
-        width = int(rect.width())
-        height = int(rect.height())
-        app.setSize(0,0, width, height)
+        root = Tk()
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        self.setGeometry(0, 0, screen_width, screen_height)
+        self.setWindowTitle('Capture screen')
         self.begin = QtCore.QPoint()
         self.end = QtCore.QPoint()
         self.setWindowOpacity(0.4)
-        QtWidgets.QApplication.setOverrideCursor(
-        QtGui.QCursor(QtCore.Qt.CrossCursor)
-        )
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        self.show()
-        self.withdraw()
-
+        self.is_snipping = False
 
     def paintEvent(self, event):
         qp = QtGui.QPainter(self)
@@ -42,9 +33,9 @@ class Snip_tool(QtWidgets.QWidget):
         qp.drawRect(QtCore.QRect(self.begin, self.end))
 
     def mousePressEvent(self, event):
-        self.begin = event.pos()
-        self.end = self.begin
-        self.update()
+        elf.begin = event.pos()
+        elf.end = self.begin
+        elf.update()
 
     def mouseMoveEvent(self, event):
         self.end = event.pos()
@@ -57,30 +48,21 @@ class Snip_tool(QtWidgets.QWidget):
         y1 = min(self.begin.y(), self.end.y())
         x2 = max(self.begin.x(), self.end.x())
         y2 = max(self.begin.y(), self.end.y())
-
+        
         img = ImageGrab.grab(bbox = (x1, y1, x2, y2))
         img_array = np.array(img)
         img = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
 
-        cv2.imshow('Captured', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = Snip_tool()
-    window.show()
-    app.aboutToQuit.connect(app.deleteLater)
-    sys.exit(app.exec_())
-
-#---------------------------------------------------------------
-class Snip_copytool(Snip_tool):
-    def __init__(self, snip_number = None, start_position = (300, 300, 350, 250)):
-        super().__init__(self)
-
         text = pytesseract.image_to_string(img)
         pc.copy(text)
         print(text)
+
+        if numpy_image is not None and snip_number is not None:
+            self.image = self.convert_numpy_img_to_qpixmap(numpy_image)
+            self.change_and_set_title("Snip #{0}".format(snip_number))
+        else:
+            self.image = QPixmap("background.PNG")
+            self.change_and_set_title(Menu.default_title)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
@@ -88,13 +70,3 @@ if __name__ == '__main__':
     window.show()
     app.aboutToQuit.connect(app.deleteLater)
     sys.exit(app.exec_())
-
-#-----------------------------------------------------------------------------------------------
-#class scan_upload(QtWidgets.QWidget):
-#    print('testing')
-#    fle = filedialog.askopenfilename(initialdir = 'C:/gui/', title = 'Open File', filetypes = (('text files', '*.txt'), ('HTML Files', '*html'), ('Python files', '*,py'), ('All files', '*.*')))
-#    text = pytesseract.image_to_string(fle)
-#    pc.copy(text)
-
-
-#-----------------------------------------------------------------------------------------------
