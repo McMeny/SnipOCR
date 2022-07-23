14eba0af98ac3966282b206ea470ef2cf5cb7063
McMeny <minuadefris@gmail.com>
2022-07-23 22:28:47 +0100

    array revamp

--- SnipOCR/engine.py
+++ SnipOCR/engine.py
@@ -6,25 +6,10 @@ import sys
 import numpy as np
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\minua\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
-import pyautogui
-import keyboard
+import struct
+import array
 
 def Snip_tool():
-    #screenshot = pyautogui.screenshot()
-    #img_array = np.array(screenshot)
-    #image = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
-    #cv2.imshow('image', image)
-    #print('screenshot taken')
-    #img = ImageTk.PhotoImage(image=Image.fromarray(img_array))
-
-    #root = Tk()
-    #screen_width = root.winfo_screenwidth()
-    #screen_height = root.winfo_screenheight()
-    #root.geometry(f'{screen_width}x{screen_height}')
-    #canvas = Canvas(root, width = screen_width, height = screen_height)
-    #canvas.pack()
-    #canvas.create_image(20, 20, anchor = 'nw', image = img)
-
     root = Tk()
     global n, coords
     n=0
@@ -45,7 +30,7 @@ def Snip_tool():
 
     root.bind('<Key>', key_press)
     root.bind('<KeyRelease>', key_release)
-
+#-----------------------------------------------------------------------------
     def cur_press_event(event):
         global n,coords,a
         if n==0:
@@ -63,20 +48,52 @@ def Snip_tool():
         n=0
         x1,y1 = event.x, event.y
         if coords is not None:
-            print(*coords)
-            print(x1,y1)
 
             root.withdraw()
+            
+            
+        def photo_image(image: np.ndarray):
+            height, width = img_array.shape[:2]
+            header = f'P6 {width} {height} 255'.encode()
+            img_data = header + cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR).tobytes()
+            img_struct = struct.unpack('f', img_data)
+            return PhotoImage(width = width, height = height, data = img_struct, format = 'PPM')
 
-            img = ImageGrab.grab(bbox= (coords[0],coords[1],x1, y1))
-            img_array = np.array(img)
-            print(type(img_array))
-            image = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
+            
+        imggrab = ImageGrab.grab(bbox= (coords[0],coords[1],x1, y1))
+        img_array = np.array(imggrab)
+        img_shape = img_array.shape[:2]
+        img = photo_image(img_array)
 
-            win = cv2.namedWindow('Snip Window')
-            cv2.imshow('Snip Window', image)
-            cv2.createButton('Snip', Snip_tool, None, cv2.QT_PUSH_BUTTON,1)
+        img_canvas = Canvas(root, width = 300, height = 300)
+        img_canvas.pack()
+        img_canvas.create_image(img_shape[0], img_shape[1], anchor = 'nw', image = img)
+
+            #win = cv2.namedWindow('Snip Window')
+            #cv2.imshow('Snip Window', image)
         x1=y1=coords=None
 
     root.bind('<B1-Motion>', cur_press_event)
     root.bind('<ButtonRelease-1>',setn)
+#-----------------------------------------------------------------------------
+    #def paint_event(event):
+    #    global p,paint_coords
+    #    if p == 0:
+    #        p += 1
+    #        paint_coords=event.x,event.y
+    #        color = (0, 255, 0)
+    #    else:
+    #        canvas.coords(paint_coords[0] ,paint_coords[1], event.x, event.y)
+
+    #def setp(event):
+    #    global p,x1,y1,paint_coords
+    #    p = 0
+    #    x1,y1 = event.x, event.y
+    #    if paint_coords is not None:
+    #        print(*paint_coords)
+    #        print(x1, y1)
+
+    #x1=y1=paint_coords=None
+
+    #root.bind('<B1-Motion>', paint_event)
+    #root.bind('<ButtonRelease-1>', setp)
